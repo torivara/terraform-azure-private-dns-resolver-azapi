9e702f3d705bc023b24587d5b9db7563f9b982e9
Tor Ivar <torivara@gmail.com>
2022-05-22 20:32:58 +0200

    Update

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "myfirsttfrg"
-  location = "australiaeast"
+  name     = "demo-azapi-rg"
+  location = "westeurope"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
-  name                = "dnsvnet"
+  name                = "demo-azapi-vnet"
   resource_group_name = azurerm_resource_group.rg.name
 }
 
